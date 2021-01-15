078078cef7f4a30ee4b4d765f1efd437836f84e6
Erik Huang <erikhuangdrums@gmail.com>
2021-01-14 23:07:26 -0800

    done

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -6,8 +8,67 @@ class Song
     @@all
   end
 
+  def self.create
+    song = self.new
+    self.all << song
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.new_by_name(name)
+    self.all << song
+    song
+  end
+
+  def self.find_by_name(name)
+    self.all.find { |song| song.name == name }
+  end
+
+  def self.find_or_create_by_name(name)
+    existing_song = self.find_by_name(name)
+    if existing_song
+      existing_song
+    else
+      self.create_by_name(name)
+    end
+  end
+
+  def self.alphabetical
+    self.all.sort_by { |song| song.name }
+  end
+
+  def self.new_from_filename(str)
+    words = str.split(" - ")
+    artist = words[0]
+    name = words[1].split(".")[0]
+
+    song = self.create_by_name(name)
+    song.artist_name = artist
+    song
+  end
+
+  def self.create_from_filename(str)
+    song = self.new_from_filename(str)
+    self.all << song
+    song
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
+
   def save
     self.class.all << self
   end
 
 end
+
+song1 = Song.new_from_filename("Taylor Swift - Blank Space.mp3")
+
+puts song1
